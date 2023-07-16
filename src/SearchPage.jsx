dc3668b54b87d1d55ec4fbf42c5c14915c7dbf71
김다인 <dbslzhs1218@naver.com>
2023-07-17 05:01:27 +0900

    feat: 헤더 생성

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 function SearchPage() {
-	return <div>SearchPage</div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 }
 export default SearchPage;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<div>
+			<h1>국내 모든 임상시험 검색하고 온라인으로 참여하기</h1>
+		</div>
+	);
+}
+export default Header;
