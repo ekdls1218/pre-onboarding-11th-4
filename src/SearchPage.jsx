00d72dd411628d7ab012bc01085ae3d545b82eab
김다인 <dbslzhs1218@naver.com>
2023-07-19 21:13:05 +0900

    style: 코드 포맷팅

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Header from './components/Header';
 import instance from './apis/axios';
 import RecKeyWord from './components/recKeyWord';
@@ -54,20 +54,16 @@ function SearchPage() {
 		const url = process.env.REACT_APP_API_BASE_URL + `?q=${input}`;
 		if (input !== '') {
 			const response = await instance.get(`?q=${input}`);
-			// setRes(response.data);
-			// console.info('calling api');
 			const cacheStorage = await caches.open('keyword');
 			const cacheResponse = await cacheStorage.match(url);
 			try {
 				if (cacheResponse) {
-					//console.log(await cacheResponse.json())
 					const SliceCache = (await cacheResponse.json()).slice(0, 7);
 					setRes(SliceCache);
 				} else {
 					const cloneResponse = new Response(JSON.stringify(response.data));
 					await cacheStorage.put(url, cloneResponse);
 					setRes(response.data.slice(0, 7));
-					console.log(res);
 					console.info('calling api');
 				}
 			} catch (error) {
@@ -86,7 +82,7 @@ function SearchPage() {
 					placeholder="검색어를 입력하세요."
 					value={input}
 					onChange={inputOnChange}
-					onKeyDown={(e) => handleKeyDown(e)}
+					onKeyDown={handleKeyDown}
 				/>
 				<SearchBtn>검색</SearchBtn>
 			</SearchBar>
